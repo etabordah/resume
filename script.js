4efc0ee39f9244701721f89c19fa6d4699e8503e
Flegerto <estiven1209@gmail.com>
2023-02-12 00:54:08 -0500

    Actualización parcial 1

--- script.js
+++ script.js
@@ -36,17 +36,47 @@ const resume = {
     'Leadership'
   ],
   languages: ['English', 'Spanish'],
-  professionalSummary: `I am a last semester Mechanical Engineering student, who has cursed part of the Maintenance Specialization and is passionate about using software and automatization. I have 5 years of experience with applying and inspecting SMAW and GMAW welds, oxi-fuel welding and other machinery and tools of metallurgy and building. I have lead groups of up to 49 people. Founder and C.E.O of Latincueros Leather Shop, were I implement LEAN manufacturing and Kaizen. I will also work to use technology to improve the customers’ buying experience at the store.`,
+  professionalSummary: 
+    '',
+  /*
+    `I am a last semester Mechanical Engineering student, who has cursed part of the Maintenance Specialization 
+    and is passionate about using software and automatization. I have 5 years of experience with applying and 
+    inspecting SMAW and GMAW welds, oxi-fuel welding and other machinery and tools of metallurgy and building. 
+    I have lead groups of up to 49 people. Founder and C.E.O of Latincueros Leather Shop, were I implement 
+    LEAN manufacturing and Kaizen. I will also work to use technology to improve the customers’ buying experience at the store.`,
+    */
   employmentHistory: [
     {
-      jobTitle: 'Co-Founder and COO',
+      jobTitle: 'Co-Founder and CEO',
       startDate: 'Sep 2019',
-      endDate: 'Now',
+      endDate: 'Jun 2021',
       employer: 'Latincueros',
       city: 'Medellín, COL',
       achievements: [
         'I co-founded a business in leather distribution',
         'Leather articles production.'
+      ]
+    },
+    {
+      jobTitle: 'Intership',
+      startDate: 'Jul 2021',
+      endDate: 'Dec 2021',
+      employer: 'Mántum S.A.S.',
+      city: 'Medellín, COL',
+      achievements: [
+        '',
+        ''
+      ]
+    },
+    {
+      jobTitle: 'Junior Consultant',
+      startDate: 'Jan 2021',
+      endDate: 'Now',
+      employer: 'Mántum S.A.S.',
+      city: 'Medellín, COL',
+      achievements: [
+        '',
+        ''
       ]
     }
   ],
