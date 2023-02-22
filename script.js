a080f6dad0b3cb1525b197188c536aaeb33c9c54
Flegerto <estiven1209@gmail.com>
2023-02-21 21:24:45 -0500

    Summary and Quria

--- script.js
+++ script.js
@@ -13,16 +13,18 @@ const resume = {
   education: [
     {
       school: 'Universidad Nacional de Colombia, Sede Medellín',
-      degree: 'Bachelor',
+      degree: 'College',
       graduationDate: '2022',
       description: 'Mechanical Engineering'
     }
   ],
   links: [
+    /*
     {
       label: 'GitHub',
       link: 'https://github.com/flegerto'
     },
+    */
     {
       label: 'LinkedIn',
       link: 'https://www.linkedin.com/in/etabordah/'
@@ -37,7 +39,7 @@ const resume = {
   ],
   languages: ['English', 'Spanish'],
   professionalSummary: 
-    '',
+    'Ingeniero Mecánico con 1-2 años en el campo de la gestión del mantenimiento, electrónica y software. Más de 5 años de experiencia encabezando negocios. Con habilidad para garantizar el avance de los proyectos, generar compromiso en proveedores y equipos de trabajo y generar el cierre de ventas. Líder en la creación de proyectos para el mejoramiento y automatización de procesos en las organizaciones.',
   /*
     `I am a last semester Mechanical Engineering student, who has cursed part of the Maintenance Specialization 
     and is passionate about using software and automatization. I have 5 years of experience with applying and 
@@ -47,36 +49,53 @@ const resume = {
     */
   employmentHistory: [
     {
+      jobTitle: 'Administrative Assistant',
+      startDate: 'Nov 2016',
+      endDate: 'Sep 2019',
+      employer: 'Pielesynegocios SAS',
+      city: 'Medellín, Col',
+      achievements: [
+        'Leather articles production.',
+        'Delivery final products and materials',
+        'Administrative assistant',
+        'Introducing process improving projects into the company'
+
+      ]
+    }
+    {
       jobTitle: 'Co-Founder and CEO',
       startDate: 'Sep 2019',
       endDate: 'Jun 2021',
       employer: 'Latincueros',
-      city: 'Medellín, COL',
+      city: 'Medellín, Col',
       achievements: [
         'I co-founded a business in leather distribution',
-        'Leather articles production.'
-      ]
-    },
-    {
-      jobTitle: 'Intership',
-      startDate: 'Jul 2021',
-      endDate: 'Dec 2021',
-      employer: 'Mántum S.A.S.',
-      city: 'Medellín, COL',
-      achievements: [
-        '',
-        ''
+        'Leather articles production.',
+        'App developing to accountability',
+        'Software developing to Measuring leather Area using computer vision',
+        'Costumers service in Spanish and English',
+        'Community manager',
       ]
     },
     {
       jobTitle: 'Junior Consultant',
-      startDate: 'Jan 2021',
+      startDate: 'Jul 2021',
       endDate: 'Now',
       employer: 'Mántum S.A.S.',
-      city: 'Medellín, COL',
+      city: 'Medellín, Col',
       achievements: [
-        '',
-        ''
+        'Coding web-Automatization codes with Selenium, Python and http requests',
+        '"Integration" Mántum CMMS with Google calendar APIs',
+        'Usage of Google cloud services',
+        'Bussiness inteligence panels in datastudio'
+        'Automatization of process and administrative documents with AppsScript',
+        'MQTT server configuration',
+        'APP developing to coordinate and document all the installation, troubleshooting and automatic generation of costumers delivery certificate',
+        'Management of an IoT project with more than 400 devices',
+        'Designing CAD, ECAD, coding and management of the fabrication of IoT Products',
+        'Closing deals worldwide',
+        'Installation of IoT products around the county',
+        'Consulting maintenance managers about the use of an CMMS and the best practices in Maintenance'
       ]
     }
   ],
